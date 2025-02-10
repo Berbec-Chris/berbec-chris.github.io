f4402e7ff3e2ad5901a5dfe9e8f2e42f3828f7f8
The Element of Surprise <60611660+Berbec-Chris@users.noreply.github.com>
2025-02-10 18:07:21 +0100

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,36 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
-/* Menu Show */
-if (navToggle) {
-    navToggle.addEventListener('click', () => {
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu');
-    });
+    })
 }
 
-/* Menu Hidden */
-if (navClose) {
-    navClose.addEventListener('click', () => {
+if(navClose){
+    navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu');
-    });
+    })
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link');
 
-const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu');
-
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu');
-};
-
-navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== SHADOW HEADER ===============*/
 
+
 /*=============== EMAIL JS ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-/*=============== DARK LIGHT THEME ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
