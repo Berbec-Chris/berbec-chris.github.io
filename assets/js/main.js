676ff6e0bbde0199cf813a0caabe38bd54a92286
The Element of Surprise <60611660+Berbec-Chris@users.noreply.github.com>
2025-02-10 17:54:42 +0100

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
+const navMenu = document.getElementById('nav-menu');
+    navToggle = document.getElementById('nav-toggle');
     navClose = document.getElementById('nav-close');
 
 /*Menu Show */
