20c70748f0295ff107fd812c7b91826fced73e4d
The Element of Surprise <60611660+Berbec-Chris@users.noreply.github.com>
2025-02-10 17:00:11 +0100

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,13 @@
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close');
-
+/*Menu Show */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu');
     })
 }
-
+/* Menu Hidden*/
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu');
