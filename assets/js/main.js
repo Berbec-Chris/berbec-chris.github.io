7cb14408129b5b934a865d7f9d7c1a6a0b09f9a6
The Element of Surprise <60611660+Berbec-Chris@users.noreply.github.com>
2025-02-10 18:03:17 +0100

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,43 +3,40 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
-/*Menu Show */
-if (navToggle){
-    navToggle.addEventListener('click', () =>{
+/* Menu Show */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu');
-    })
-};
-/* Menu Hidden*/
-if (navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu');
-    })
-};
-
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
-
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+    });
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
-/*=============== SHADOW HEADER ===============*/
+/* Menu Hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*=============== SHADOW HEADER ===============*/
 
 /*=============== EMAIL JS ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
